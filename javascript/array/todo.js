a7329b2198917604225591ae87b6ec511036dd7c
Shawn Loh <shawnloh1996@gmail.com>
2016-11-20 13:09:58 +0800

    Added some array

--- javascript/array/todo.js
+++ javascript/array/todo.js
@@ -5,13 +5,35 @@ var input = prompt("What would you like to do?");
 while (input !== "quit")
 {
 	if (input === "list") {
-		console.log(todos);
+		listToDos();
 	} else if (input === "new") {
-	//ask for new todo
-	var newToDo = prompt("Enter new todo");
-	todos.push(newToDo)
-}
-input = prompt("What would you like to do?");
+		addToDo();
+	}else if (input === "delete"){
+		deleteToDo();
+	}
+	input = prompt("What would you like to do?");
 
 }
 console.log("Ok, YOU QUIT THE APP");
+
+function listToDos() {
+	console.log("**********");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("***********");
+};
+
+function addToDo() {
+		//ask for new todo
+		var newToDo = prompt("Enter new todo");
+		todos.push(newToDo);
+	};
+
+	function deleteToDo() {
+	//ask for index of todo to be deleted
+	var index = prompt("Enter index of todo to delete");
+	//delete that todo
+	//splice
+	todos.splice(index, 1);
+};
